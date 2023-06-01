a0ddcd89c67d232d3d0b2c8615e669a67ae4fa0a
Gustavo Henrick <gustavo.napolitano@ws-solution.com>
2023-06-01 20:28:15 -0300

    feat: Changed List Companies Endpoint

--- lib/src/shared/helpers/endpoints/endpoints.dart
+++ lib/src/shared/helpers/endpoints/endpoints.dart
@@ -8,7 +8,7 @@ abstract class Endpoints {
   static String users = "$baseURL/users";
   static String jobs = "$baseURL/jobs";
   static String jobsById(String jobId) => "$baseURL/jobs/id/$jobId";
-  static String companies = "$baseURL/companies";
+  static String companies = "$baseURL/companies-list";
 
   static String forgotPassword = "$baseURL/reset-password-email";
   static String resetPassword = "$baseURL/reset-password";
@@ -16,7 +16,7 @@ abstract class Endpoints {
   static String getJobsByRecruiterId(String userId) =>
       "$baseURL/jobs?created_by=$userId";
   static String pageJobs(int page, String userId) =>
-      "$baseURL/jobs?page=$page&limit=10&created_by=$userId";
+      "$baseURL/jobs/v2?page=$page&limit=10&created_by=$userId";
   static String companyImage = "$baseURL/companies-image";
 
   static String getJobById(String jobId) => "$baseURL/jobs/id/$jobId";
